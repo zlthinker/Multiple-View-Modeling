bdb8c24053e81d77e4ff72df2cc6ed97782794df
zlthinker <zhouleibetter@163.com>
2016-05-09 14:27:33 +0800

    + Complete the pipeline and add image write

--- NormalOptimization.cpp
+++ NormalOptimization.cpp
@@ -23,7 +23,7 @@ double DataFn(int p, int l, void* data)
 
 double SmoothFn(int p1, int p2, int l1, int l2, void* data)
 {
-    double lambda = 0.001;
+    double lambda = 0.01;
     double sigma = 1;
     NormDataFn* norm_data = (NormDataFn*) data;
     cv::Vec3d label_norm1 = (norm_data->label_norm)[l1];
@@ -31,7 +31,7 @@ double SmoothFn(int p1, int p2, int l1, int l2, void* data)
     return lambda * log(1 + cv::norm(label_norm1 - label_norm2) / (2 * pow(sigma, 2)));
 }
 
-void NormalOptimization(std::vector<cv::Vec3d> & estim_norm, std::vector<cv::Vec3d> & label_norm, std::vector<int> & label_id,
+void NormalOptimization(std::vector<cv::Vec3d> & estim_norm, std::vector<cv::Vec3d> & label_norm,
                         int width, int height, std::vector<int> & results)
 {
     int num_pixels = estim_norm.size();
@@ -51,7 +51,7 @@ void NormalOptimization(std::vector<cv::Vec3d> & estim_norm, std::vector<cv::Vec
         printf("\nAfter optimization energy is %f",gc->compute_energy());
 
         for ( int  i = 0; i < num_pixels; i++ )
-            results[i] = gc->whatLabel(i);
+            results.push_back(gc->whatLabel(i));
 
         delete gc;
 
@@ -59,6 +59,30 @@ void NormalOptimization(std::vector<cv::Vec3d> & estim_norm, std::vector<cv::Vec
     catch (GCException e){
         e.Report();
     }
+}
 
+bool VisualizeNorm(std::vector<cv::Vec3d>& norms, cv::Mat image)
+{
+    int width = image.cols;
+    int height = image.rows;
+    if (norms.size() < width * height)
+    {
+        std::cerr << "Normal vector is wrong.\n";
+        return false;
+    }
+
+    cv::Vec3d light(0.5, 0, std::sqrt(3) / 2);
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            int id = h * width + w;
+            cv::Vec3d & norm_vector = norms[id];
+            int color = (int)128 * (norm_vector[0] * light[0] + norm_vector[1] * light[1] + norm_vector[2] * light[2]);
+            image.at<uchar>(h, w) = color;
+        }
+    }
+
+    return true;
 
 }
--- main.cpp
+++ main.cpp
@@ -2,48 +2,106 @@
 #include "GCO/energy.h"
 #include "GCO/GCoptimization.h"
 #include <opencv/highgui.h>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-void NormalOptimization(std::vector<cv::Vec3d> & estim_norm, std::vector<cv::Vec3d> & label_norm, std::vector<int> & label_id,
+void NormalOptimization(std::vector<cv::Vec3d> & estim_norm, std::vector<cv::Vec3d> & label_norm,
                         int width, int height, std::vector<int> & results);
 int GCO_main(int argc, char **argv);
+bool VisualizeNorm(std::vector<cv::Vec3d>& norms, cv::Mat image);
 
 int main() {
 
-    /*
-    int width = 10;
-    int height = 5;
-    int num_pixels = width*height;
-    int num_labels = 7;
-
+    int width, height;
+    int num_pixels;
+    int num_labels;
     std::vector<cv::Vec3d> estim_norms;
     std::vector<cv::Vec3d> label_norms;
-    std::vector<int> label_ids;
+    std::vector<cv::Vec3d> opti_norms;
     std::vector<int> results;
+
+
+    // read estimated normals
+    std::string estim_norm_file = "/Users/Larry/HKUST/Lessons/COMP5421_Computer_Vision/project_4/data/norm/initalNorm9.txt";
+    std::ifstream f_norm(estim_norm_file);
+    if (f_norm.is_open())
+    {
+        f_norm >> width >> height;
+    }
+
+    num_pixels = width * height;
     estim_norms.reserve(num_pixels);
-    label_norms.reserve(num_labels);
-    label_ids.reserve(num_pixels);
+    opti_norms.reserve(num_pixels);
     results.reserve(num_pixels);
 
-    for (int i = 0; i < num_pixels; i++)
+    while (f_norm.is_open() && !f_norm.eof())
     {
-        estim_norms.push_back(cv::Vec3d(i, i, i));
-        label_ids.push_back(i % num_labels);
+        double x, y, z;
+        f_norm >> x >> y >> z;
+        estim_norms.push_back(cv::Vec3d(x, y, z));
+    }
+    f_norm.close();
+    std::cout << "Reading estimated normal file finished.\n";
+    estim_norms.resize(num_pixels);
+
+    // read norm label
+    std::string label_norm_file = "/Users/Larry/HKUST/Lessons/COMP5421_Computer_Vision/project_4/data/semi-sphere.txt";
+    std::ifstream f_label(label_norm_file);
+    while (f_label.is_open() && !f_label.eof())
+    {
+        double x, y, z;
+        f_label >> x >> y >> z;
+        label_norms.push_back(cv::Vec3d(x, y, z));
+    }
+    f_label.close();
+    std::cout << "Reading label normal file finished.\n";
+    label_norms.resize(label_norms.size() - 1);
+    num_labels = label_norms.size();
+
+    // before optimization
+    cv::Mat init_image(height, width, CV_8UC1);
+    if (!VisualizeNorm(estim_norms, init_image))
+    {
+        std::cerr << "Fail to visualize normal vectors.\n";
+        return -1;
+    }
+    std::string image_before = "/Users/Larry/HKUST/Lessons/COMP5421_Computer_Vision/project_4/data/norm/init_image8.png";
+    cv::imwrite(image_before, init_image);
+
+//    cv::namedWindow("Before optimization", cv::WINDOW_NORMAL);
+//    cv::imshow("Before optimization", init_image);
+
+    NormalOptimization(estim_norms, label_norms, width, height, results);
+
+    // save optimized normal
+    std::string opti_norm_file = "/Users/Larry/HKUST/Lessons/COMP5421_Computer_Vision/project_4/data/norm/optimalNormal8.txt";
+    std::ofstream f_opti(opti_norm_file);
+
+    if (!f_opti.is_open())
+    {
+        std::cout << "Fail to open " << opti_norm_file << '\n';
+        return -1;
+    }
+    f_opti << width << ' ' << height << '\n';
+    for (int i = 0; i < results.size(); i++)
+    {
+        cv::Vec3d & opti_vector = label_norms[ results[i] ];
+        opti_norms.push_back(opti_vector);
+        f_opti << opti_vector[0] << ' ' << opti_vector[1] << ' ' << opti_vector[2] << '\n';
     }
 
-    for (int i = 0; i < num_labels; i++)
+    // after optimization
+    cv::Mat opti_image(height, width, CV_8UC1);
+    if (!VisualizeNorm(opti_norms, opti_image))
     {
-        label_norms.push_back(cv::Vec3d(i, i, i));
+        std::cerr << "Fail to visualize normal vectors.\n";
+        return -1;
     }
-
-    NormalOptimization(estim_norms, label_norms, label_ids, width, height, results);
-     */
-
-    char ** c;
-    GCO_main(0, c);
-
-
+//    cv::imshow("After optimization", opti_image);
+    std::string image_after = "/Users/Larry/HKUST/Lessons/COMP5421_Computer_Vision/project_4/data/norm/opti_image8.png";
+    cv::imwrite(image_after, opti_image);
 
 
     return 0;
